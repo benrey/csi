c1e7996ff87f4f69bb3b2ae00c804109a221f1b3
Ben Rey <brey@JBI.local>
2022-02-28 00:18:23 -0600

    changes to navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(window).scroll(function() {
   if ($(document).scrollTop() > 20) {
     $('nav').addClass('transparent');
+    $('con').addClass('transparent')
   } else {
     $('nav').removeClass('transparent');
+    $('con').removeClass('transparent');
   }
 });
 
